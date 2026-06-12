eb8d1b4764e3dc5a97809d2724e18f4d0ef09545
Joshimani <caraiglex@gmail.com>
2026-06-12 09:34:25 -0300

    Modified solvespace

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+!!i5r^f14n^j_c$+=*u2%k%g%=&zlklrx72hs(no8-rwkc@9^'
+SECRET_KEY = os.getenv('django-insecure-+!!i5r^f14n^j_c$+=*u2%k%g%=&zlklrx72hs(no8-rwkc@9^')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = [
-    '*'
-]
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 STATIC_URL = 'static/'
 
