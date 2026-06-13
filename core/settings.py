87e839764de5d4cc8af550851b00a6b3fc62b8e1
Joshimani <caraiglex@gmail.com>
2026-06-13 11:39:05 -0300

    Modified my solvespace project to deploy on render

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+!!i5r^f14n^j_c$+=*u2%k%g%=&zlklrx72hs(no8-rwkc@9^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.onrender.com',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Application definition
